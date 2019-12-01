c089720f73f91993846145185ed2806be5cd02a6
adamdacosta97 <56278548+adamdacosta97@users.noreply.github.com>
2019-12-01 17:20:04 -0500

    Day 5
    Art, Menu, and wave spawner

---
+++ Project2/Assets/Scripts/Wave.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public GameObject enemy;
+    public int count;
+    public float rate;
+}
