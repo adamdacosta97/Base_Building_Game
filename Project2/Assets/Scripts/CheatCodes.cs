9b66b050c761dddcaef29572af207a29df87b4d2
adamdacosta97 <56278548+adamdacosta97@users.noreply.github.com>
2019-12-05 18:08:47 -0500

    Day 7
    Cheat Codes, and Enemy Cave

---
+++ Project2/Assets/Scripts/CheatCodes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCodes : MonoBehaviour
+{
+    //Checks to see if a cheat key is pressed and activates the cheat
+    void Update()
+    {
+        if(Input.GetKeyDown("c"))
+        {
+            PlayerStats.Money += 100;
+        }
+        if(Input.GetKeyDown("l"))
+        {
+            GaveOver.lives = 900;
+        }
+    }
+}
